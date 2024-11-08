1be0e88857ec52fa7b5bd9dc76aca9d62af3524d
lorax <142108266+lorax42@users.noreply.github.com>
2024-11-08 08:42:13 +0100

    update project structure

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #! /usr/bin/bash
 
-cmake -S ./src -B ./build
+a="$(ls)"
+b="$(ls ../group_generator/)"
+
+if [[ $a != $b ]]; then
+    echo "E: not in project root dir"
+    exit 1
+fi
+
+if [[ ! -d ./build/ ]]; then
+    mkdir build/
+fi
+
+cd build/
+cmake .. && make
